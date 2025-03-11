87126405b446b43d8673d87cd8ced48c5b358027
Rathin Rachhadiya <123184952+rathin1514@users.noreply.github.com>
2025-03-11 23:39:51 +0100

    Update index.js

--- react-fitness/src/index.js
+++ react-fitness/src/index.js
@@ -14,7 +14,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename="/">
+    <BrowserRouter>
         <AuthProvider>
             <App />
         </AuthProvider>
